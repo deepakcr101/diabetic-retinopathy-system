58c4488dd64d54d1fa45ef4501a1f6eb0b9a90b5
deepak <piomega01@gmail.com>
2025-12-13 15:21:20 +0530

    project restructured

--- backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('ping/', views.ping, name='ping'),
-    path('predict/', views.predict_view, name='predict'),
-    path('results/', views.results_view, name='results'),
-]
